3fa16c60c8eec882c13915be596bd835320a1869
Revanth Shahukaru <rshahuka@syr.edu>
2024-12-04 00:20:44 +0000

    Revanth made some changes and added page3-whisper.py and created a new file for page4-support.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
-
+# streamlit_app.py
 import streamlit as st
 from streamlit_option_menu import option_menu
+
 # Set page configuration (must be the first Streamlit command)
 st.set_page_config(page_title="Interactive Travel Guide Chatbot", page_icon="🌎", layout="wide")
 page1 = st.Page("page1.py", title="Explore")
 page2 = st.Page("page2.py", title="Itenary")
-pg = st.navigation([page1, page2])
+page3 = st.Page("page3-whisper.py", title="Translator")
+pg = st.navigation([page1, page2, page3])
 pg.run()
